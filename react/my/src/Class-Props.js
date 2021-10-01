317c407d2adc067516d13eb485c49da99e6ea51a
DARSHANMJ2027 <darshanmj234@gmail.com>
2021-10-01 19:23:05 +0530

    ok

---
+++ react/my/src/Class-Props.js
@@ -0,0 +1,10 @@
+import React from "react";
+class Pros extends React.Component{
+    Name='Darshan'
+    Age = '23'
+    City = 'Davanagere'
+    render(){
+        return <div> {this.Pros.Name}</div>
+    }
+}
+export default Pros;
